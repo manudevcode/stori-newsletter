5843ba1943b17d1dd1e698860b36ca73a6888b81
manudevcode <manyoh.3y@gmail.com>
2024-06-23 22:56:56 -0600

    fix: paths

--- src/services/api/routes.ts
+++ src/services/api/routes.ts
@@ -1,7 +1,7 @@
-export const NEWSLETTERS = "/newsletter";
-export const RECIPIENTS = "/recipients";
-export const RECIPIENTS_USERS = "/recipients/users";
-export const RECIPIENT_USER = "/unsubscribe/by-email/";
-export const AVAILABLE_LISTS = "/unsubscribe/available-lists";
-export const UNSUBSCRIBE = "/unsubscribe";
-export const AUTH = "/auth";
+export const NEWSLETTERS = "newsletter";
+export const RECIPIENTS = "recipients";
+export const RECIPIENTS_USERS = "recipients/users";
+export const RECIPIENT_USER = "unsubscribe/by-email/";
+export const AVAILABLE_LISTS = "unsubscribe/available-lists";
+export const UNSUBSCRIBE = "unsubscribe";
+export const AUTH = "auth";
